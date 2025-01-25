ab7905ae87704dd21ae1db1c4de857ce392199c3
aliaksandrrachko <rachko.a.ch@gmail.com>
2025-01-25 18:32:23 +0100

    change size to count func

--- src/main/java/com/metanit/collections/immmtableandmutables/set.kt
+++ src/main/java/com/metanit/collections/immmtableandmutables/set.kt
@@ -2,7 +2,7 @@ package com.metanit.collections.immmtableandmutables
 
 fun main() {
     val items = setOf(1, 2, 3, 4, 5)
-    println(items.size)          // 5
+    println(items.count())          // 5
     println(items.contains(4))  // true
     println(items.isEmpty())    // false
     println(items.minus(3))     // [1, 2, 4, 5]
